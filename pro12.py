8502c3b139ae05f852a2c72400617bd9a40cd7ee
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-06-02 16:10:12 +0530

    Create pro12.py

---
+++ pro12.py
@@ -0,0 +1,21 @@
+n,q=map(int,input().split())
+li=list(map(int,input().split()))
+li1=[]
+l=[]
+k=[]
+for i in range(0,q):
+  u,v=map(int,input().split())
+  for i in range(u,v+1):
+    li1.append(li[i-1])
+  l.append(sum(li1))
+print(l)  
+k.append(l[0])
+for i in range(0,len(l)-1):
+  s=l[i+1]-l[i]
+  k.append(s)
+for i in k:
+  print(i)
+
+
+
+
