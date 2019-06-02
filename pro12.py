efa7b896b316dbb2244b455b0321f154690fd052
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-06-02 16:12:06 +0530

    Update pro12.py

--- pro12.py
+++ pro12.py
@@ -7,8 +7,7 @@ for i in range(0,q):
   u,v=map(int,input().split())
   for i in range(u,v+1):
     li1.append(li[i-1])
-  l.append(sum(li1))
-print(l)  
+  l.append(sum(li1)) 
 k.append(l[0])
 for i in range(0,len(l)-1):
   s=l[i+1]-l[i]
